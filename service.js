6bb65a4b0ede9280fc1fc40cc0dd92a4150b827c
Janis Krasemann <janis.krasemann@enpit.de>
2016-07-14 23:44:18 +0200

    use express instead of restify, implement the remaining regexps and routes

--- service.js
+++ service.js
@@ -1,7 +1,10 @@
-const restify = require('restify');
+const express = require('express');
 
 const PORT = process.env.PORT || 8083;
-const server = restify.createServer();
+const server = express();
+
+const httpRegexp = /^\/(http:\/\/www\.[\w]+\.com)$/;
+const shortenedUrlRegexp = /^\/(\w+)$/;
 
 const shorten = (url) => 'Error: Not implemented yet';
 
@@ -11,12 +14,20 @@ server.use((req, res, next) => {
   next();
 });
 
-server.get('/:url', (req, res) => {
-  const url = req.params.url;
+server.get(httpRegexp, (req, res) => {
+  const url = req.params[0];
   res.json({
     original_url: url,
     short_url: shorten(url)
   });
+});
+
+server.get(shortenedUrlRegexp, (req, res) => {
+  res.send('shortened urls soon coming to a location near you');
+});
+
+server.all('*', (req, res) => {
+  res.send('invalid URL');
 });
 
 server.listen(PORT, () => console.log('url-komprimator listening on ' + PORT));
